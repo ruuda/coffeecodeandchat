bb132c411ec5db8dfe1b0401f8b563e589e12340
Ruud van Asseldonk <dev@veniogames.com>
2025-11-15 13:20:10 +0100

    Make script rewrite the index page

--- update.py
+++ update.py
@@ -6,14 +6,22 @@ Update the dates on the webpage.
 
 from datetime import date
 
+import re
+
 
 epoch = date(2025, 11, 15).toordinal()
 offset = epoch % 14
 today = date.today().toordinal()
 
-dates = [
-    date.fromordinal((((today - offset) // 14) + i) * 14 + offset)
+dates = iter([
+    str(date.fromordinal((((today - offset) // 14) + i) * 14 + offset))
     for i in range(0, 3)
-]
+])
 
-print(dates)
+DATE = r"[0-9]{4}-[0-9]{2}-[0-9]{2}"
+
+
+with open("index.new.html", "w", encoding="utf-8") as fo:
+    with open("index.html", "r", encoding="utf-8") as fi:
+        for line in fi:
+            fo.write(re.sub(DATE, lambda _match: next(dates), line))
