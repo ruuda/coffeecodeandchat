143370800de2c68b8026cce14858379d69586873
Ruud van Asseldonk <dev@veniogames.com>
2025-11-27 19:57:01 +0100

    Correct off-by-one, update index page for this week

--- update.py
+++ update.py
@@ -6,6 +6,7 @@ Update the dates on the webpage.
 
 from datetime import date
 
+import os
 import re
 
 
@@ -15,13 +16,15 @@ today = date.today().toordinal()
 
 dates = iter([
     str(date.fromordinal((((today - offset) // 14) + i) * 14 + offset))
-    for i in range(0, 3)
+    for i in range(1, 4)
 ])
 
 DATE = r"[0-9]{4}-[0-9]{2}-[0-9]{2}"
 
 
-with open("index.new.html", "w", encoding="utf-8") as fo:
-    with open("index.html", "r", encoding="utf-8") as fi:
+with open("index.html", "r", encoding="utf-8") as fi:
+    with open("index.new.html", "w", encoding="utf-8") as fo:
         for line in fi:
             fo.write(re.sub(DATE, lambda _match: next(dates), line))
+
+os.rename("index.new.html", "index.html")
